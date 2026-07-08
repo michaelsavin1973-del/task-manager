b2c1f83f9b1c609f9d1c877cbcf27b2a7ccda29d
michaelsavin <michaelsavin1973@gmail.com>
2026-07-08 22:13:09 +1200

    Deploy task manager to Kubernetes with PostgreSQL

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,35 @@
+import os
+
 from flask import Flask
-from app.routes.task_routes import task_bp
+
+from app.db.db import db
+
 
 def create_app():
     app = Flask(__name__)
+
+    database_url = os.getenv("DATABASE_URL")
+
+    if not database_url:
+        database_url = (
+            f"postgresql://{os.environ['DB_USER']}:"
+            f"{os.environ['DB_PASSWORD']}@"
+            f"{os.environ['DB_HOST']}:5432/"
+            f"{os.environ['DB_NAME']}"
+        )
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    # импортируем модели после init_app
+    from app.models.task import Task
+
+    with app.app_context():
+        db.create_all()
+
+    from app.routes.task_routes import task_bp
     app.register_blueprint(task_bp)
+
     return app
